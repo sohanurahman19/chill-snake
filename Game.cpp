1a7413d01688905493a40626429b3359cd4259ca
sohanurahman19 <sohanurr337@gmail.com>
2026-06-29 17:30:26 +0600

    wrote constructor and destructor for the game

--- Game.cpp
+++ Game.cpp
@@ -1,6 +1,17 @@
 #pragma once
 #include <game.hpp>
 
+game::game() : painter(nullptr), window(nullptr) { };
+game::~game(){
+     if(window != nullptr){
+          SDL_DestroyWindow(window);
+     }
+     if(painter != nullptr){
+          SDL_DestroyRenderer(painter);
+     }
+     SDL_Quit();
+}
+
 bool game::init(){
      if(SDL_Init(SDL_INIT_VIDEO) != 0) return false;
      window = SDL_CreateWindow("Chill Game", config::screen_width, config::screen_height, 0);
