21eb3edd163c5dbed3d5d1a7f5c5118096d78708
sohanurahman19 <sohanurr337@gmail.com>
2026-06-29 15:38:53 +0600

    fixed typo at currrentpos.y

--- Food.cpp
+++ Food.cpp
@@ -10,5 +10,5 @@ void Food::re_spawn(){
      int unit_x = config::screen_width / config::grid_size;
      int unit_y = config::screen_height / config::grid_size;
      current_pos.x = (rand() % unit_x) * config::grid_size;
-     current_pos.y = (rand() & unit_y) * config::grid_size;
+     current_pos.y = (rand() % unit_y) * config::grid_size;
 }
