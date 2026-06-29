ae1bd8599febf138f3d85057737431ec4edbcf22
sohanurahman19 <sohanurr337@gmail.com>
2026-06-29 16:47:46 +0600

    wrote game.hpp and trying to write game.cpp

--- Game.hpp
+++ Game.hpp
@@ -1,6 +1,21 @@
 #pragma once
+#include <SDL3/SDL.h>
+#include <Snake.hpp>
+#include <Food.hpp>
 
-class Game{
+class game{
+private:
+     SDL_Window* window;
+     SDL_Renderer* painter;
+     snake python;
+     food fish;
+
+     void handle_input();
+     void update();
+     void paint();
 public:
-     
+     game();
+     ~game();
+     bool init();
+     void run();
 };
